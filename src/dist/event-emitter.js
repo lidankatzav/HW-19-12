d3cf1d1c23058d5116f561d9f0c1de4ffdbeb773
lidankatzav <lidankatzav5@gmail.com>
2022-12-20 20:54:54 +0200

    full OOP & DS

--- src/dist/event-emitter.js
+++ src/dist/event-emitter.js
@@ -1,9 +1,15 @@
 var EventEmitter = /** @class */ (function () {
+    // { eventName: [callbacks functions], ... }
     function EventEmitter(callbacksListPerEvent) {
         if (callbacksListPerEvent === void 0) { callbacksListPerEvent = {}; }
         this.callbacksListPerEvent = callbacksListPerEvent;
     }
     EventEmitter.prototype.subscribe = function (eventName, callbackFunc) {
+        // This method "subscribe" a callback function to some event.
+        // Checking first if the event type is already exists in the dict, 
+        // if not - adding the event (as a key) and the callback func in array (as value) to the dict.
+        // Also checking if the callback func is alreay "subscribed" to the event. If it's, do nothing.
+        // Returns an object with "unsubscribe" func.
         var _this = this;
         if (eventName in this.callbacksListPerEvent) {
             if (!this.callbacksListPerEvent[eventName].includes(callbackFunc)) {
@@ -19,6 +25,9 @@ var EventEmitter = /** @class */ (function () {
         };
     };
     EventEmitter.prototype.emit = function (eventName, eventProps) {
+        // This method call to all the funcs that "subscribed" to some event,
+        // by iterating the array of all functions linked to the same event in the dict.
+        // Each func gets the event props as an argument. 
         if (eventName in this.callbacksListPerEvent) {
             this.callbacksListPerEvent[eventName].forEach(function (callbackFunc) {
                 return callbackFunc(eventProps);
@@ -26,6 +35,9 @@ var EventEmitter = /** @class */ (function () {
         }
     };
     EventEmitter.prototype.unsubscribe = function (eventName, callbackFuncToRemove) {
+        // This method does "unsubscribe" to some callback func linked to some event,
+        // by deleting the callback func from the funcs array of the event in the dict.
+        // If the event has no linked functions left - delete this event from the dict.
         this.callbacksListPerEvent[eventName] = this.callbacksListPerEvent[eventName].filter(function (callbackFunc) { return callbackFunc !== callbackFuncToRemove; });
         if (this.callbacksListPerEvent[eventName].length === 0) {
             this.callbacksListPerEvent[eventName] = undefined;
@@ -34,13 +46,17 @@ var EventEmitter = /** @class */ (function () {
     };
     return EventEmitter;
 }());
+// Main 
+// Testing on two callback funcs
 var logFunc1 = function (props) {
     console.log(props.key);
 };
 var logFunc2 = function (props) {
     console.log(props.key);
 };
+// Create a new EventEmitter
 var eventEmitter = new EventEmitter();
+// Create two subscriptions to two funcs above with the event 'keydown'
 var keydownSubscription1 = eventEmitter.subscribe('keydown', logFunc1);
 var keydownSubscription2 = eventEmitter.subscribe('keydown', logFunc2);
 eventEmitter.emit('keydown', { key: 'Enter' });
@@ -48,3 +64,15 @@ keydownSubscription1.unsubscribe();
 eventEmitter.emit('keydown', { key: 'Enter' });
 var keydownSubscription3 = eventEmitter.subscribe('keydown', logFunc1);
 eventEmitter.emit('keydown', { key: 'Enter' });
+// Expected output ---->
+/*
+{ keydown: [ [Function: logFunc1] ] }
+{ keydown: [ [Function: logFunc1], [Function: logFunc2] ] }
+Enter
+Enter
+{ keydown: [ [Function: logFunc2] ] }
+Enter
+{ keydown: [ [Function: logFunc2], [Function: logFunc1] ] }
+Enter
+Enter
+*/
--- src/event-emitter.ts
+++ src/event-emitter.ts
@@ -1,13 +1,21 @@
 
 class EventEmitter {
 
-    private callbacksListPerEvent: { [key: string]: Function[] }; // { eventName: [callbacks functions], ... }
+    private callbacksListPerEvent: { [key: string]: Function[] }; 
+    // { eventName: [callbacks functions], ... }
     
     constructor(callbacksListPerEvent =  {}) {
         this.callbacksListPerEvent = callbacksListPerEvent;
     }
 
     subscribe(eventName: string, callbackFunc: Function): {unsubscribe: Function} {
+
+        // This method "subscribe" a callback function to some event.
+        // Checking first if the event type is already exists in the dict, 
+        // if not - adding the event (as a key) and the callback func in array (as value) to the dict.
+        // Also checking if the callback func is alreay "subscribed" to the event. If it's, do nothing.
+        // Returns an object with "unsubscribe" func.
+
         if(eventName in this.callbacksListPerEvent) {
             if(!this.callbacksListPerEvent[eventName].includes(callbackFunc)) {
                 this.callbacksListPerEvent[eventName].push(callbackFunc);
@@ -23,6 +31,11 @@ class EventEmitter {
     }
 
     emit(eventName: string, eventProps: {[key: string]: string}): void {
+
+        // This method call to all the funcs that "subscribed" to some event,
+        // by iterating the array of all functions linked to the same event in the dict.
+        // Each func gets the event props as an argument. 
+
         if(eventName in this.callbacksListPerEvent) {
             this.callbacksListPerEvent[eventName].forEach((callbackFunc) => 
             callbackFunc(eventProps))
@@ -30,6 +43,11 @@ class EventEmitter {
     }
 
     unsubscribe(eventName: string, callbackFuncToRemove: Function): void {
+
+        // This method does "unsubscribe" to some callback func linked to some event,
+        // by deleting the callback func from the funcs array of the event in the dict.
+        // If the event has no linked functions left - delete this event from the dict.
+        
         this.callbacksListPerEvent[eventName] = this.callbacksListPerEvent[eventName].filter((callbackFunc) => callbackFunc !== callbackFuncToRemove);
         if(this.callbacksListPerEvent[eventName].length === 0) {
             this.callbacksListPerEvent[eventName] = undefined;
@@ -37,6 +55,10 @@ class EventEmitter {
         console.log(this.callbacksListPerEvent);
     }
 }
+
+// Main 
+
+// Testing on two callback funcs
 
 const logFunc1 = (props: { key: string; }): void => {
     console.log(props.key);
@@ -46,7 +68,11 @@ const logFunc2 = (props: { key: string; }): void => {
     console.log(props.key);
 }
 
+// Create a new EventEmitter
+
 const eventEmitter: EventEmitter = new EventEmitter();
+
+// Create two subscriptions to two funcs above with the event 'keydown'
 
 const keydownSubscription1 = eventEmitter.subscribe('keydown', logFunc1);
 const keydownSubscription2 = eventEmitter.subscribe('keydown', logFunc2);
@@ -57,4 +83,16 @@ eventEmitter.emit('keydown', {key: 'Enter'});
 const keydownSubscription3 = eventEmitter.subscribe('keydown', logFunc1);
 eventEmitter.emit('keydown', {key: 'Enter'});
 
+// Expected output ---->
 
+/*
+{ keydown: [ [Function: logFunc1] ] }
+{ keydown: [ [Function: logFunc1], [Function: logFunc2] ] }
+Enter
+Enter
+{ keydown: [ [Function: logFunc2] ] }
+Enter
+{ keydown: [ [Function: logFunc2], [Function: logFunc1] ] }
+Enter
+Enter
+*/
